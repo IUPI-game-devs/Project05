7174725f45afbbac8a6b02738851824b9db2f1e9
Luis <luisfernandojavier.velazquez@upr.edu>
2023-10-18 22:08:44 -0400

    Added correct C# script

--- Project 05/Assets/Scripts/ShooterMovInput.cs
+++ Project 05/Assets/Scripts/ShooterMovInput.cs
@@ -5,45 +5,99 @@ using UnityEngine.InputSystem;
 
 public class ShooterMovInput : MonoBehaviour
 {
-
     public float speed;
-    public float rotationSpeed;
+    public float speedRotation;
     private Vector2 movementValue;
     private float lookValue;
     public GameObject prefab;
     public GameObject shootPoint;
+    public float dashMulti;
+    public float jumpValue;
+
+    private bool isGrounded;
+    private int jumpsLeft = 2; 
+
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    private void Awake(){
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
-
-    public void OnMove(InputValue value){
+    // Moving
+    public void OnMove(InputValue value)
+    {
         movementValue = value.Get<Vector2>() * speed;
     }
 
-    public void OnLook(InputValue value){
-        lookValue = value.Get<Vector2>().x * rotationSpeed;
+    // Looking
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>().x * speedRotation;
     }
 
-    public void OnFire(InputValue value){
-        if(value.isPressed){
+    // Shooting
+    public void OnFire(InputValue value)
+    {
+        if (value.isPressed)
+        {
             GameObject clone = Instantiate(prefab);
             clone.transform.position = shootPoint.transform.position;
             clone.transform.rotation = shootPoint.transform.rotation;
         }
     }
-
+    // Dash
+    public void OnDash()
+    {
+        movementValue = movementValue * dashMulti;
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movementValue.x * Time.deltaTime, 0, movementValue.y * Time.deltaTime);
+        transform.Translate(
+            movementValue.x * Time.deltaTime,
+            0,
+            movementValue.y * Time.deltaTime);
         transform.Rotate(0, lookValue * Time.deltaTime, 0);
+    }
+
+// Jumping
+    public void OnJump(InputValue value)
+    {
+        if (isGrounded || jumpsLeft > 0)
+        {
+            if (value.isPressed)
+            {
+                // Reset vertical velocity before jumping
+                GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, 0f, GetComponent<Rigidbody>().velocity.z);
+                
+                // Perform the jump
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
+
+                // Reduce available jumps if double jumping
+                if (!isGrounded)
+                {
+                    jumpsLeft--;
+                }
+            }
+        }
+    }
+
+// Can detect ground
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpsLeft = 1; // Reset available jumps when grounded
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
